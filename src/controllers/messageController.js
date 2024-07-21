b08471872ef7163e972737f17cb519a72897bf6a
Luka <lukhanyo95@gmail.com>
2024-07-21 16:01:46 +0200

    add route

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,8 +1,8 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const emailMessage = (req, res) => {
-  const { email, subject, message } = req.body;
+const emailMessage = async (req, res) => {
+  const { senderName, email, subject, message } = req.body;
 
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_SERVER,
@@ -13,7 +13,24 @@ const emailMessage = (req, res) => {
       pass: process.env.SMTP_PASSWORD,
     },
   });
-  
+
+  const mailOptions = {
+    from: `${senderName} ${email}`,
+    to: "luka.matshebelele@gmail.com",
+    subject: subject,
+    text: message,
+    html: `<b>${message}</b>`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(500).send("Can't receive email");
+      console.log(error);
+    } else {
+      res.status(200).send("Email received!");
+      console.log("Received email from", info.accepted);
+    }
+  });
 };
 
-module.exports = {emailMessage}
+module.exports = { emailMessage };
---
+++ src/routes/messageRoute.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const {emailMessage} = require("../controllers/messageController")
+
+router.post("/send-message", emailMessage)
+
+module.exports = {router}
