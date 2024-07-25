65c86ed614ae23bd415398886d7dbfe669eda694
Luka <lukhanyo95@gmail.com>
2024-07-25 16:39:22 +0200

    remove unnecesary comments

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const messageRoute = require("./src/routes/messageRoute");
-require("dotenv").config();
-
 const port = 3300;
 
+app.use(express.json());
 app.use(cors());
 
 const allowedOrigin = "http://localhost:4200";
@@ -15,23 +15,18 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   }
 
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-access-token"
+  );
+  
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
   next();
 });
-
-app.use(express.json());
-
-// app.use((_req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, x-access-token"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, OPTIONS"
-//   );
-//   next();
-// });
 
 app.get("/", (req, res) => {
   res.status(200).send("The server is running...");
