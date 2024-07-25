618ac7da99d3250098f9d78fc92eac56ab6c6b70
Luka <lukhanyo95@gmail.com>
2024-07-25 16:53:36 +0200

    update the allowed origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3300;
 app.use(express.json());
 app.use(cors());
 
-const allowedOrigin = "http://localhost:4200";
+const allowedOrigin = "https://portfolio-angular-taupe.vercel.app/portfolio";
 
 app.use((req, res, next) => {
   if (req.origin === allowedOrigin) {
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, x-access-token"
   );
-  
+
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
